3a560f5c00accc3b469b4ab36f06ab187be3c3e1
c4ldeira27 <c4ldeira27@gmail.com>
2026-09-12 13:02:53 -0300

    Other fixes

--- tests/orquestrator.js
+++ tests/orquestrator.js
@@ -11,6 +11,7 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
+      // eslint-disable-next-line no-unused-vars
       const responseBody = response.json();
     }
   }
