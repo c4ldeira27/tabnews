b340a9edacb55d0da9c891e307295aee88ec9f13
c4ldeira27 <c4ldeira27@gmail.com>
2026-09-13 13:59:33 -0300

    test: rename all descriptions to the new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,11 +6,15 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET TO /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(response.status).toBe(200);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
